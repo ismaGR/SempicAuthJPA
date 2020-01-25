9d0dd3cc7307f65de60f54140f741856e48fdb97
whosbaX <whosbax@gmail.com>
2020-01-26 00:17:07 +0100

    fix albums

--- src/main/java/fr/uga/miashs/sempic/backingbeans/CreateAlbum.java
+++ src/main/java/fr/uga/miashs/sempic/backingbeans/CreateAlbum.java
@@ -12,7 +12,11 @@ import fr.uga.miashs.sempic.entities.Photo;
 import fr.uga.miashs.sempic.entities.SempicUser;
 import fr.uga.miashs.sempic.qualifiers.Selected;
 import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import java.util.List;
+import java.util.Map;
 
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
@@ -83,18 +87,26 @@ public class CreateAlbum implements Serializable{
 
     public String save(){
         try {
-            String albumIdRequest = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("albumId");
+            Map<String,String> requestParams = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+            String albumIdRequest=requestParams.get("albumId");            
+            //String albumIdRequest = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("albumId");
+            System.out.println("album id from request");
+            System.out.println(albumIdRequest);
             albumId = Long.parseLong(albumIdRequest);
             Album album=service.read(albumId);
             album.setGroups(current.getGroups());       
-            service.update(album);
+            Logger.getLogger(SessionTools.class.getName()).log(Level.INFO, "albumIdRequest: "+albumIdRequest+" requestParams"+requestParams, "albumIdRequest: "+albumIdRequest+" requestParams"+requestParams);
+            Logger.getLogger(SessionTools.class.getName()).log(Level.INFO, "album: "+album, "album: "+album);
+            if(album != null && service.update(album) instanceof Album){
+                return "success";
+            }
             //st.setCurrentAlbum(current);
         } catch (SempicModelException ex) {
            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(ex.getMessage()));
             return "failure";
         }
         
-        return "success";        
+        return "failure";        
     }
     public String create() {
         //System.out.println(current);
